e5c35546e28ff7cbb0619ffdcf917fc7765767a8
Stealcannon977 <84484913+Stealcannon977@users.noreply.github.com>
2025-08-05 19:11:28 -0400

    Add files via upload

---
+++ oliver-cromwell/modules/ui/game.js
@@ -0,0 +1,9 @@
+import LeaderModelManager from '/base-standard/ui/diplomacy/leader-model-manager.js';
+
+LeaderModelManager.modelMap = {
+    ...(LeaderModelManager.modelMap || {}),
+    // LEADER_CROMWELL: "CHAR_JACOBIN_BILLAUD_VARENNE"
+    // LEADER_CROMWELL: "CHAR_JACOBIN_DESMOULINS"
+    LEADER_CROMWELL: "CHAR_JACOBIN_SAINT_JUST"
+    // LEADER_CROMWELL: "CHAR_CONQUISTADOR_CORTES"
+}
